37dbf6abeddd520e2904a9a8c1f9de659c319273
GitHub1043 <69536208+GitHub1043@users.noreply.github.com>
2022-12-16 13:34:57 -0600

    finished cleanup

--- videos.js
+++ videos.js
@@ -1,19 +1,19 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
+$(document).ready(function () {
+  $("select")
+    .change(function () {
+      $(this)
+        .find("option:selected")
+        .each(function () {
+          var optionValue = $(this).attr("value");
+          if (optionValue) {
+            $(".videos")
+              .not("." + optionValue)
+              .hide();
+            $("." + optionValue).show();
+          } else {
+            $(".videos").hide();
+          }
+        });
+    })
+    .change();
+});
