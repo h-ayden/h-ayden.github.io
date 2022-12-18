7ba23a5c6cd74475f789b72136431768e8073e66
GitHub1043 <69536208+GitHub1043@users.noreply.github.com>
2022-12-17 22:24:53 -0600

    code cleanup and separation

---
+++ preload.js
@@ -0,0 +1,4 @@
+$(window).on("load", function () {
+  $("body").removeClass("preload");
+  $(".loader-wrapper").fadeOut("fast,linear");
+});
--- videos.js
+++ videos.js
@@ -19,10 +19,10 @@ $(document).ready(function () {
     .change();
 });
 
+// Collapsible
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
-// Collapsible
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
